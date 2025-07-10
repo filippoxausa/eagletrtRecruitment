58e2dc4b71a6fb4a7b0cd39d4ab9da60bb0cec0b
filippoxausa <filippoxausa@gmail.com>
2025-07-10 13:13:53 +0200

    Adjusted LIMIT_DIST_X

--- include/cone_detection.hpp
+++ include/cone_detection.hpp
@@ -5,7 +5,7 @@
 #define LIMIT_HEIGHT_HIGH 365 // Altezza minima in cui rilevare i coni
 #define LIMIT_HEIGHT_LOW 210 // Altezza massima in cui rilevare i coni
 #define LIMIT_DIST_NORM 80.0 // Distanza massima in cui sovrapporre i coni
-#define LIMIT_DIST_X 2.0 // Distanza massima (orizzontale) in cui sovrapporre i coni
+#define LIMIT_DIST_X 3.0 // Distanza massima (orizzontale) in cui sovrapporre i coni
 
 
 std::vector<cv::Point> getConeCenters(const cv::Mat& mask, double areaMin = 25.0, double areaMax = 1200.0);
